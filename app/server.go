43a98015e13ceb1acfc3745a0dea37ca20a2c224
claudealdric <cvaldric@gmail.com>
2024-08-16 19:19:56 -0400

    Add functionality to read headers on the `/user-agent` endpoint

--- app/server.go
+++ app/server.go
@@ -42,27 +42,60 @@ func main() {
 func processRequest(conn net.Conn) {
 	defer conn.Close()
 
-	requestLine, err := bufio.NewReader(conn).ReadString('\n')
+	reader := bufio.NewReader(conn)
+
+	requestLine, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Println("Error reading request: ", err.Error())
 		return
 	}
 
-	path := extractPath(requestLine)
-	routeRequest(conn, path)
+	headers, err := getHeaders(reader)
+	if err != nil {
+		fmt.Println("Error reading headers: ", err.Error())
+		return
+	}
 
+	path := extractPath(requestLine)
+	routeRequest(conn, path, headers)
+}
+
+func getHeaders(reader *bufio.Reader) (map[string]string, error) {
+	headers := make(map[string]string)
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+
+		if line == "\r\n" {
+			break
+		}
+
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) == 2 {
+			key := strings.ToLower(strings.TrimSpace(parts[0]))
+			value := (strings.TrimSpace(parts[1]))
+			headers[key] = value
+		}
+	}
+
+	return headers, nil
 }
 
 func extractPath(requestLine string) string {
 	return strings.Fields(requestLine)[1]
 }
 
-func routeRequest(conn net.Conn, path string) {
+func routeRequest(conn net.Conn, path string, headers map[string]string) {
 	switch {
 	case path == "/":
 		handleRoot(conn)
 	case strings.HasPrefix(path, "/echo/"):
 		handleEcho(conn, path)
+	case strings.HasPrefix(path, "/user-agent"):
+		handleUserAgent(conn, headers)
 	default:
 		handleNotFound(conn)
 	}
@@ -91,6 +124,17 @@ func handleEcho(conn net.Conn, path string) {
 	buildStatusLine(&response, http.StatusOK)
 	buildPlainTextHeaders(&response, echoArg)
 	response.WriteString(echoArg)
+
+	fmt.Fprint(conn, response.String())
+}
+
+func handleUserAgent(conn net.Conn, headers map[string]string) {
+	userAgent := headers["user-agent"]
+
+	var response strings.Builder
+	buildStatusLine(&response, http.StatusOK)
+	buildPlainTextHeaders(&response, userAgent)
+	response.WriteString(userAgent)
 
 	fmt.Fprint(conn, response.String())
 }
