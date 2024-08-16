7c4ec1c265a50b2c9f3eacd7fb7685c06b08b8db
claudealdric <cvaldric@gmail.com>
2024-08-16 16:31:50 -0400

    Rename the variables to be more meaningful

--- app/server.go
+++ app/server.go
@@ -9,17 +9,17 @@ import (
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	c, err := l.Accept()
+	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	fmt.Fprint(c, "HTTP/1.1 200 OK\r\n\r\n")
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n\r\n")
 }
